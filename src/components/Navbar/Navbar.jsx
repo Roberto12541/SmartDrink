316f3ff4fc86aec9bf6f10f5036fbef52edb2902
Miguel Chan <miguelchan2002@outlook.es>
2023-06-19 13:44:46 -0600

    second cmmit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <section>
+        <header>
+            <nav>
+                <ul>
+                    <li>Inicio</li>
+                    <li>About</li>
+                    <li>Products</li>
+                    <h1>Commmerce X</h1>
+                    <li>Pages</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    </section>
+
+  )
+}
+
+export default Navbar
