9482083a1d5a4e5afdc28d398ee7579cf542386a
Roberto12541 <robertomiranda1254@gmail.com>
2023-06-20 00:24:14 -0600

    React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Navbar from './components/Navbar/Navbar.jsx'
 import './index.css'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Navbar />
-  </React.StrictMode>,
+  // <React.StrictMode>
+    <App />
+  // </React.StrictMode>
 )
