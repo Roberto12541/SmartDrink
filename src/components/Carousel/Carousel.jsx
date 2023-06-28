2f6c4c24e1056c6c49f478dbf5957b228da0452e
Roberto12541 <robertomiranda1254@gmail.com>
2023-06-27 22:30:26 -0600

    fixed carrusel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -9,7 +9,7 @@ const Carrusel = () => {
     return (
         <div id="indicators-carousel" className="relative w-full" data-carousel="slide">
             <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
-                <div className="hidden duration-700 ease-in-out" data-carousel-item="active">
+                <div className="duration-700 ease-in-out" data-carousel-item="active">
                     <img src={item1} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
                 </div>
                 <div className="hidden duration-700 ease-in-out" data-carousel-item>
