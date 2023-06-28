d96f8a1e6f4412d4e42dc41b5cd027f2a3a85b4e
Roberto12541 <robertomiranda1254@gmail.com>
2023-06-27 19:50:08 -0600

    Logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
           <li><NavLink to={'/contacto'}>Contacto</NavLink></li>
         </ul>
         <Link to={'/'}>
-          <img src="./assets/Logo.png" className='h-14 lg:block' alt="" />
+          <img src="./public/Logo.png" className='h-14 lg:block' alt="" />
         </Link>
         <div className="flex items-center gap-4">
           <button type="button" className="relative inline-flex items-center py-2.5 text-sm font-medium text-center cursor-pointer">
