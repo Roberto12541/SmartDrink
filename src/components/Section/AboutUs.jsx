8ce2ecd9d78352ef2966279d377d587bb5f4aa48
Miguel Chan <miguelchan2002@outlook.es>
2023-07-03 16:31:53 -0600

    elpapi

--- src/components/Section/AboutUs.jsx
+++ src/components/Section/AboutUs.jsx
@@ -5,11 +5,12 @@ const AboutUs = () => {
                 <div className="flex flex-col items-start bg-black justify-center px-4 py-24 lg:px-20">
                     <span className="mb-4 p-2 px-4 rounded-xl bg-white badge text-black font-semibold">SmartDrink</span>
                     <h1 className="mb-6 text-4xl font-bold leading-tight text-white md:text-4xl lg:text-5xl">¿Quienes Somos?</h1>
-                    <p className="pr-0 mb-4 text-lg text-white tracking-relaxed text-justify lg:pr-16">Somos un equipo de desarrolladores que busca
-                        innovar y agilizar la preparacion de bebidas
-                        de nuestros clientes con metodos tecnológicos
-                        ya que queremos que nuestros clientes se posicionen
-                        entre los mejores siendo originales y autenticos.</p>
+                    <p className="pr-0 mb-4 text-lg text-white tracking-relaxed text-justify lg:pr-16">Somos un equipo de jovenes desarrolladores que busca
+                        facilitar y agilizar la preparacion de bebidas
+                        de nuestros clientes con metodos tecnologicos
+                        ya que nuestro principal objetivo es que nuestros clientes se posicionen
+                        entre los mejores siendo distintos a los demas.
+                    </p>
                 </div>
                 <div>
                     <img src="https://s1.1zoom.me/big0/126/Drinks_Strawberry_Cocktail_Black_background_548048_1280x893.jpg" alt="" className="object-cover w-full h-64 md:h-full" loading="lazy" />
@@ -22,8 +23,8 @@ const AboutUs = () => {
                 <div className="flex flex-col items-start justify-center px-4 py-24 lg:px-20 order-1 md:order-2">
                     <h1 className="mb-6 text-4xl font-bold leading-tight text-black md:text-4xl lg:text-5xl">Misión y Visión</h1>
                     <p className="pr-0 mb-4 text-lg text-black tracking-relaxed text-justify lg:pr-24">Ser reconocidos a nivel nacional e internacional
-                        siendo originales, profesionales y teniendo la mejor
-                        calidad humana y principios eticos para ofrecer nuestros
+                        siendo diferentes a los demas, teniendo la mejor
+                        calidad posible y principios eticos para ofrecer nuestros
                         servicios y productos de excelencia.</p>
                 </div>
             </section>
--- src/components/Section/Team.jsx
+++ src/components/Section/Team.jsx
@@ -4,16 +4,16 @@ const Team = () => {
             <div className="container px-5 py-24 mx-auto">
                 <div className="flex flex-col text-center w-full mb-20">
                     <h1 className="text-2xl font-medium title-font mb-4 text-white tracking-widest">NUESTRO EQUIPO</h1>
-                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-white">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam repellat eligendi sequi cupiditate, excepturi omnis?</p>
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-white">JAVATEC Equipo de jovenes desarrolladores.</p>
                 </div>
                 <div className="flex flex-wrap -m-4">
                     <div className="p-4 lg:w-1/2">
                         <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
                             <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://cdn-icons-png.flaticon.com/512/149/149071.png" />
                             <div className="flex-grow sm:pl-8">
-                                <h2 className="title-font font-medium text-lg text-white">Miguel Angel</h2>
+                                <h2 className="title-font font-medium text-lg text-white">Miguel Angel Chan V.</h2>
                                 <h3 className="text-gray-500 mb-3">Lider de proyecto</h3>
-                                <p className="mb-4 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita perspiciatis consequatur ea odit dicta eius!</p>
+                                <p className="mb-4 text-white">Encargado de dirigir el proyecto.</p>
                                 <span className="inline-flex">
                                     <a className="text-gray-500">
                                         <svg fill="none" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-5 h-5" viewBox="0 0 24 24">
@@ -38,9 +38,9 @@ const Team = () => {
                         <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
                             <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://cdn-icons-png.flaticon.com/512/149/149071.png" />
                             <div className="flex-grow sm:pl-8">
-                                <h2 className="title-font font-medium text-lg text-white">Roberto Miranda</h2>
+                                <h2 className="title-font font-medium text-lg text-white">Roberto Adrian Miranda M.</h2>
                                 <h3 className="text-gray-500 mb-3">Desarrollador</h3>
-                                <p className="mb-4 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita perspiciatis consequatur ea odit dicta eius!</p>
+                                <p className="mb-4 text-white">Encargado de desarrollar.</p>
                                 <span className="inline-flex">
                                     <a className="text-gray-500">
                                         <svg fill="none" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-5 h-5" viewBox="0 0 24 24">
@@ -65,9 +65,9 @@ const Team = () => {
                         <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
                             <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://cdn-icons-png.flaticon.com/512/149/149071.png" />
                             <div className="flex-grow sm:pl-8">
-                                <h2 className="title-font font-medium text-lg text-white">Marco Antonio</h2>
+                                <h2 className="title-font font-medium text-lg text-white">Marco Antonio Torres L.</h2>
                                 <h3 className="text-gray-500 mb-3">UI Developer</h3>
-                                <p className="mb-4 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita perspiciatis consequatur ea odit dicta eius!</p>
+                                <p className="mb-4 text-white">Encargado de nada ijo de su ptmadre</p>
                                 <span className="inline-flex">
                                     <a className="text-gray-500">
                                         <svg fill="none" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-5 h-5" viewBox="0 0 24 24">
@@ -92,9 +92,9 @@ const Team = () => {
                         <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
                             <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://cdn-icons-png.flaticon.com/512/149/149071.png" />
                             <div className="flex-grow sm:pl-8">
-                                <h2 className="title-font font-medium text-lg text-white">Jose Luis</h2>
+                                <h2 className="title-font font-medium text-lg text-white">Jose Luis Oro C.</h2>
                                 <h3 className="text-gray-500 mb-3">Designer</h3>
-                                <p className="mb-4 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita perspiciatis consequatur ea odit dicta eius!</p>
+                                <p className="mb-4 text-white">otro igual pero menos pendejo</p>
                                 <span className="inline-flex">
                                     <a className="text-gray-500">
                                         <svg fill="none" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-5 h-5" viewBox="0 0 24 24">
